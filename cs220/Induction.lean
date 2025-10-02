4eacbe2f5ffacee0328e733f937f13c2a76b6f7c
AliBighdeli0 <alibigdeli66@gmail.com>
2025-10-02 13:59:04 -0600

    [cs220_lean] init files

---
+++ cs220/Induction.lean
@@ -0,0 +1,31 @@
+import Mathlib
+
+/-- Theorem: For all natural numbers n ≥ 4, 4^n ≥ n^4. --/
+theorem exp_ge_pow (n : ℕ) (hn : 4 ≤ n) : 4^n ≥ n^4 := by
+  induction n, hn using Nat.le_induction
+
+  case base =>
+    -- Goal: 4^4 ≥ 4^4
+    rfl
+
+  case succ n hn ih =>
+    -- Goal : 4^(n+1) ≥ (n+1)^4
+    -- Hypotheses: hn : n ≥ 4 and ih : 4^n ≥ n^4
+    calc 4^(n + 1)
+      _ ≥ 4 * n^4           := by omega -- applies ih for us since it's of the right form
+      _ ≥ (n + 1)^4         := by -- a tricky calculation...
+          calc 4 * n^4
+          _ = n^4 + n^4 + n^4 + n^4 := by ring
+          _ ≥ n^4 + 4*n^3 + 6*n^2 + (4*n + 1) := by
+            gcongr
+            -- have hn : 4 ≤ n so a single nlinarith should sort out all these goals, no?
+            . -- goal is 4*n^3 ≤ n^4
+              have : 4 * n^3 ≤ n * n^3 := by gcongr
+              nlinarith
+            . -- goal is 6*n^2 ≤ n^4
+              have : 6 ≤ n^2 := by nlinarith
+              nlinarith
+            . -- goal is 4*n+1 ≤ n^4
+              have : 4*n ≤ n^2 := by nlinarith
+              nlinarith
+          _ = (n + 1)^4 := by ring
